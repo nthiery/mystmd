58894c2f69f62ba24d0cecde757ff16cd71ecd98
Angus Hollands <goosey15@gmail.com>
2024-06-19 13:40:38 +0100

    feat: add upgrade process

---
+++ packages/myst-cli/src/utils/git.ts
@@ -0,0 +1,16 @@
+
+import { makeExecutable } from 'myst-cli-utils';
+import { fsExists } from './fsExists.js';
+
+export async function checkFolderIsGit(): Promise<boolean> {
+  try {
+    await makeExecutable('git status', null)();
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function checkAtGitRoot(): Promise<boolean> {
+  return await fsExists(".git");
+}
